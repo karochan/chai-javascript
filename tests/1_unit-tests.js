1f9baba7976e90f9aace5143245ecac1f816690e
Karolina Krzysztofiak-Petryna <karo_chan@MacBook-Pro-Karolina.local>
2022-02-01 22:59:40 +0100

    testggfdd

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -33,7 +33,7 @@ suite('Unit Tests', function () {
   suite('Equality', function () {
     // #5
     test('#equal, #notEqual', function () {
-      assert.equal(12, '12', 'Numbers are coerced into strings with ==');
+      assert.notEqual(12, '12', 'Numbers are coerced into strings with ==');
       assert.equal({ value: 1 }, { value: 1 }, '== compares object references');
       assert.notEqual(6 * '2', '12');
       assert.notEqual(6 + '2', '12');
